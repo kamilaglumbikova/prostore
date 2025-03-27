1f1eb5cfb4e10d8f050bd4d6942f1a6b970284b6
Kamila Glumbikova <k.glumbikova@seznam.cz>
2025-03-27 17:07:43 +0100

    Sign Up Form & Error Handling

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -7,3 +7,10 @@ export const signInDefaultValue={
     email: '',
     password: ''
 }
+
+export const signUpDefaultValue={
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
