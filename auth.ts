c5dc882eedab43afadfe93a4d1ec65784fb420fe
Kamila Glumbikova <k.glumbikova@seznam.cz>
2025-03-27 17:22:43 +0100

    Customize Token With JWT Callback

--- auth.ts
+++ auth.ts
@@ -50,10 +50,11 @@ export const config = {
     })],
     callbacks: {
         async session({session, user, trigger, token}: any) {
+
             // Set the user id from the token
-            if(user) {
-                session.user.id = user.id 
-            }
+            session.user.id = token.sub;
+            session.user.role = token.role;
+            session.user.name = token.name;
 
             // if there is an update, set the user name
             if(trigger === 'update') {
@@ -61,6 +62,23 @@ export const config = {
             }
 
             return session;
+        },
+
+        async jwt({token, user,trigger, session}: any) {
+            // Assign user fields to token
+            if(user) {
+                token.role = user.role;
+                // if has no name
+                if(user.name === 'NO_NAME') {
+                    token.name = user.email!.split('@')[0];
+                    // update database
+                    await prisma.user.update({
+                        where: {id: user.id},
+                        data: {name: token.name}
+                    })
+                }
+            }
+            return token;
         }
     }
 } satisfies NextAuthConfig;
